35eb584ac9ffa542349e923f05acb7d9b135a885
Andrew Antonitis <andrewan@mit.edu>
2016-12-24 23:11:40 -0500

    Changed initial version number

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 from distutils.extension import Extension
 
 setup(name='Nescient',
-      version='0.4.5',
+      version='0.2.0',
       description='Encrypt and decrypt single files to/from secure containers',
       long_description='Python program for (un)packing encrypted, salted, and authenticated single-file containers',
       author='Andrew Antonitis',
       author_email='andrewan@mit.edu',
       packages=['nescient', 'nescient.aes'],
       ext_modules=[Extension('nescient.aes.aes', ['nescient/aes/aes.c'])],
-      scripts=['bin/nescient']
+      scripts=['bin/nescient'],
+      license='MIT'
       )
