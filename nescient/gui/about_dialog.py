58c4e4b1495992a978fb3a882e2bb1db9c0473f4
Ariel Antonitis <arant@mit.edu>
2019-06-17 02:58:01 -0400

    About window now tracks version number.

--- nescient/gui/about_dialog.py
+++ nescient/gui/about_dialog.py
@@ -1,4 +1,5 @@
 from nescient.gui.about_base import Ui_Dialog
+from nescient import __version__
 
 from PyQt5 import QtWidgets
 
@@ -8,4 +9,5 @@ class AboutNescient(QtWidgets.QDialog):
         QtWidgets.QDialog.__init__(self)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.ui.label_2.setText('Nescient v{}'.format(__version__))
         self.setFixedSize(self.size())
